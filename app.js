7b7825131f0313a78055e7c4429388366a9c5029
Virat Kamble <viratkamble19@gmail.com>
2020-07-01 17:17:15 +0530

    The very first working app prototype

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ const app=express();
 app.get("/", function(req,res){
     res.render("landing.ejs");
 });
+
+app.get("/campgrounds", function(req,res){
+    const camps=[
+        {
+        name:"Spiti Valley, Himachal Pradesh",
+        image:"https://toib.b-cdn.net/wp-content/uploads/2017/08/spiti-valley-himachal-pradesh.jpg"
+        },
+        {
+            name:"Chandratal Lake, Himachal Pradesh",
+            image:"https://toib.b-cdn.net/wp-content/uploads/2017/08/chandratal-lake-himachal-pradesh.jpg"
+        },
+        {
+            name:"Solang Valley, Manali",
+            image:"https://toib.b-cdn.net/wp-content/uploads/2017/08/solang-valley-manali.jpg"
+        },
+
+        {
+            name:"Tso Moriri, Ladakh",
+            image:"https://toib.b-cdn.net/wp-content/uploads/2017/08/tso-Moriri-Ladakh.jpg"
+        }
+    ];
+    res.render("campgrounds.ejs",{camps:camps});
+});
+
+app.listen(3000);
