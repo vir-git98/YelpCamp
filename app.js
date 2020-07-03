365d7ebf0bd2d8a9cd16515916b2ac0e1c0097e8
Virat Kamble <viratkamble19@gmail.com>
2020-07-03 19:51:57 +0530

    Connected the app to a MongoDB database

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
+//Packages
 const express=require("express");
 const ejs=require("ejs");
 const app=express();
-app.set('view engine', 'ejs');
-var bodyParser = require ("body-parser");
+const bodyParser = require ("body-parser");
+const mongoose=require("mongoose");
+
+//MongoDB connection
+mongoose.connect('mongodb://localhost/yelpcamp', {useUnifiedTopology: true, useNewUrlParser: true});
+
 app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+//MongoDB schema
+const campgroundsSchema = new mongoose.Schema({
+    name: String,
+    image:String
+});
+
+//MongoDB model
+const Campground= mongoose.model('Campground', campgroundsSchema);
+
+//MongoDB DB
+/*
+Campground.create(
+    {
+        name:"Spiti Valley, Himachal Pradesh",
+        image:"https://toib.b-cdn.net/wp-content/uploads/2017/08/spiti-valley-himachal-pradesh.jpg"
+    },
+    function(err, campgrounds){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(campgrounds);
+        }
+    }
+);
+
 
 const camps=[
     {
@@ -24,22 +56,36 @@ const camps=[
         image:"https://toib.b-cdn.net/wp-content/uploads/2017/08/tso-Moriri-Ladakh.jpg"
     }
 ];
-
+*/
 
 app.get("/", function(req,res){
     res.render("landing.ejs");
 });
 
 app.get("/campgrounds", function(req,res){
-    res.render("campgrounds.ejs",{camps:camps});
+    Campground.find({},function(err, campgrounds){
+            if(err){
+                console.log(err);
+            }else{
+                res.render("campgrounds.ejs",{camps:campgrounds});
+            }
+        }
+    );    
 });
 
 app.post("/campgrounds", function(req,res){
     const name=req.body.campname;
     const url=req.body.campurl;
     const newCamp={name:name,image:url};
-    camps.push(newCamp);
-    res.redirect("/campgrounds");
+    console.log(newCamp);
+    Campground.create(newCamp,function(err, campgrounds){
+            if(err){
+                console.log(err);
+            }else{
+                res.redirect("/campgrounds");
+            }
+        }
+    );
 });
 
 app.get("/campgrounds/new", function(req,res){
